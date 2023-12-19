88e64338b953468dec51abf15c429517a01f5884
vashuguptada <vashugupta.da@gmail.com>
2023-12-19 15:04:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+# type: ignore
+import streamlit as st
+
+from utils import get_answer_csv
+
+st.header("Chat with any CSV")
+csv_path = "D:\OS\chat-with-csv-main - Copy\chat-with-csv-main\sale.csv"
+# uploaded_file = st.file_uploader("Upload a csv file", type=["csv"])
+
+# if uploaded_file is not None:
+#     query = st.text_area("Ask any question related to the document")
+#     button = st.button("Submit")
+#     if button:
+#         st.write(get_answer_csv(uploaded_file, query))
+query = st.text_area("Ask any question related to the document")
+button = st.button("Submit")
+if button:
+    st.write(get_answer_csv(csv_path, query))
