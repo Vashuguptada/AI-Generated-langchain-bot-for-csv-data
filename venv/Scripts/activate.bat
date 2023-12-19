963de840e9ec2a64a063f36f70ec923a1dba43c6
vashuguptada <vashugupta.da@gmail.com>
2023-12-19 16:08:51 +0530

    add

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\OS\chat-with-csv-main\chat-with-csv-main\venv
+set VIRTUAL_ENV=D:\AI\solved langchain streamlit chatbot\langchain-bot\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
